361c76a95464163ab3cbba7e06ff7df4eeb2b969
Jesse Eikema <jesse@grr.nl>
2014-04-16 16:10:37 +0200

    m

--- public/js/src/modules/imageManipulation.js
+++ public/js/src/modules/imageManipulation.js
@@ -48,7 +48,7 @@ function antiAlias(newImg,storage, i){
 
 	octx.drawImage(oc, 0, 0, oc.width * 1.5, oc.height * 1.5);
 
-	//  canvas.width = inputWidth.value;
+	// canvas.width = inputWidth.value;
 	// canvas.height = inputHeight.value;
 
 	ctx.drawImage(oc, 0, 0, oc.width * 1.5, oc.height * 1.5,
