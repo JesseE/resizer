f1f54dd6de22d103b76679ac0c672b2da45e4084
Jesse Eikema <jesse@grr.nl>
2014-04-22 15:01:01 +0200

    highlight + metadata

--- public/js/src/modules/imageManipulation.js
+++ public/js/src/modules/imageManipulation.js
@@ -10,7 +10,8 @@ function manipulateImages(storage, i) {
 		storage[i],
 		function(newImg){
 			templatingCanvas(newImg, storage, i);
-			// antiAlias(newImg, storage, i);
+			 // antiAlias(newImg, storage, i);
+			imageDimensions(newImg, storage, i);
 		},
 		{
 			minWidth: inputWidth.value,
@@ -34,35 +35,68 @@ function cropImage(storage, i){
 		}
 	);
 }
-function antiAlias(newImg,storage, i){
+function imageDimensions(newImg, storage, i) {
 	var canvas = document.getElementById("canvas" + i);
-   	var ctx	= canvas.getContext("2d");
+	var div = document.createElement('div');
+	div.setAttribute("class", "dimensions");
+	div.innerHTML = ['<p>actual width: <span class="highlight">',canvas.width,'px</span>',' and actual height: <span class="highlight">',canvas.height,'px</span> </p>'].join('');
+	var item = document.getElementById("item" + i);
+	item.setAttribute("class", "item");
+	item.setAttribute("width", ""+canvas.width);
+	item.setAttribute("height", ""+canvas.height);
+	item.insertBefore(div, null);
+	//get the canvas el widht + height
+	//add the dimensions div el
+	//insert the widht and height values
+	//insert in the div
 
-<<<<<<< HEAD
-	var oc  = document.createElement('canvas'),
-    octx = oc.getContext('2d');
-=======
-		savable(aSaveMultiData);
-
-	}
-	 //crop function
-	function cropAction() {
-
-		var bSaveMultiData = [];
->>>>>>> feature/prototype
-
-	oc.width  = newImg.width * 1.5;
-	oc.height = newImg.height * 1.5;
-
-	octx.drawImage(newImg, 0, 0, oc.width, oc.height);
-
-	octx.drawImage(oc, 0, 0, oc.width * 1.5, oc.height * 1.5);
-
-	// canvas.width = inputWidth.value;
-	// canvas.height = inputHeight.value;
-
-	ctx.drawImage(oc, 0, 0, oc.width * 1.5, oc.height * 1.5,
-                  0, 0, canvas.width,   canvas.height);
 }
+// function antiAlias(newImg,storage, i){
+// 	var canvas = document.getElementById("canvas" + i);
+//    	var ctx	= canvas.getContext("2d");
+//    	console.log(canvas.toDataURL());
+//    	var NewImg = canvas.toDataURL();
+//    	NewImg = new Image();
+//     console.log("blurry bitch");
+
+// 	var oc = document.createElement('canvas'),
+//     octx = oc.getContext('2d');
+
+//     oc.width = inputWidth.value * 0.5;
+//     oc.height = inputHeight.value * 0.5;
+//     octx.drawImage(newImg,0,0, oc.width,oc.height);
+
+//     /// step 2
+//     octx.drawImage(oc,0,0,oc.width * 0.5,oc.height * 0.5);
+
+//     canvas.width=inputWidth.value;
+//     canvas.height=inputHeight.value;
+//     console.log(canvas.width);
+//     console.log(canvas.height);
+
+
+
+//     ctx.drawImage(oc,0,0,oc.width * 0.5, oc.height * 0.5,
+//                      0,0,canvas.width,canvas.height);
+// 	  var oc  = document.createElement('canvas'),
+//    octx = oc.getContext('2d');
+
+	// oc.width  = newImg.width * 1.5;
+	// oc.height = newImg.height * 1.5;
+
+	// octx.drawImage(newImg, 0, 0, oc.width, oc.height);
+
+	// octx.drawImage(oc, 0, 0, oc.width * 1.5, oc.height * 1.5);
+
+	// // canvas.width = inputWidth.value;
+	// // canvas.height = inputHeight.value;
+
+	// ctx.drawImage(oc, 0, 0, oc.width * 1.5, oc.height * 1.5,
+ //                  0, 0, canvas.width,   canvas.height);
+
+
+//}
+
+
 // problem with resizing image is that the image gets pixalated when using canvas
 // this is happening because canvas uses image smoothing or Anti aliasing which makes the edges pixalated
